e81b1fe2a2d2cb802e58bab06cb596f16d172d76
PoroCoco <48033370+PoroCoco@users.noreply.github.com>
2021-08-31 12:37:21 +0200

    Add files via upload

--- autoInstall.lua
+++ autoInstall.lua
@@ -1,12 +1,12 @@
 local shell = require("shell")
 local filesystem = require("filesystem")
 
-filesystem.makeDirectory("home/myaenetwork")
-shell.setWorkingDirectory("home/myaenetwork/")
+filesystem.makeDirectory("/home/myaenetwork")
+shell.setWorkingDirectory("/home/myaenetwork/")
 
 print("downloading")
 shell.execute("wget https://raw.githubusercontent.com/PoroCoco/myaenetwork/main/account.lua")
 shell.execute("wget https://raw.githubusercontent.com/PoroCoco/myaenetwork/main/web.lua")
-shell.setWorkingDirectory("home/")
+shell.setWorkingDirectory("/home/")
 filesystem.remove("autoInstall.lua")
 print("done")
