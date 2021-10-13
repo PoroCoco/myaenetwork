a62748367395d51a2766c0ed98e3d1e4290c0b8d
PoroCoco <48033370+PoroCoco@users.noreply.github.com>
2021-10-13 15:01:56 +0200

    Changed Base64 encoding function.
    New encoding function should be faster and so it should stop the yield error of some users.

--- account.lua
+++ account.lua
@@ -8,22 +8,41 @@ local webIdPath = "/home/myaenetwork/webIdentification.txt"
 local workDirectory = "/home/myaenetwork/"
 local newDirectory = "/home/myaenetwork"
 
---Taken from StackOverflow : https://stackoverflow.com/questions/34618946/lua-base64-encode
-local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/' -- You will need this for encoding/decoding
--- encoding
-    function encodeBase64(data)
-        return ((data:gsub('.', function(x) 
-            local r,b='',x:byte()
-            for i=8,1,-1 do r=r..(b%2^i-b%2^(i-1)>0 and '1' or '0') end
-            return r;
-        end)..'0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
-            if (#x < 6) then return '' end
-            local c=0
-            for i=1,6 do c=c+(x:sub(i,i)=='1' and 2^(6-i) or 0) end
-            return b:sub(c+1,c+1)
-        end)..({ '', '==', '=' })[#data%3+1])
-    end
---
+function lsh(value,shift)
+	return (value*(2^shift)) % 256
+end
+
+-- shift right
+function rsh(value,shift)
+	return math.floor(value/2^shift) % 256
+end
+
+-- return single bit (for OR)
+function bit(x,b)
+	return (x % 2^b - x % 2^(b-1) > 0)
+end
+
+-- logic OR for number values
+function lor(x,y)
+	result = 0
+	for p=1,8 do result = result + (((bit(x,p) or bit(y,p)) == true) and 2^(p-1) or 0) end
+	return result
+end
+
+-- encryption table
+local base64chars = {[0]='A',[1]='B',[2]='C',[3]='D',[4]='E',[5]='F',[6]='G',[7]='H',[8]='I',[9]='J',[10]='K',[11]='L',[12]='M',[13]='N',[14]='O',[15]='P',[16]='Q',[17]='R',[18]='S',[19]='T',[20]='U',[21]='V',[22]='W',[23]='X',[24]='Y',[25]='Z',[26]='a',[27]='b',[28]='c',[29]='d',[30]='e',[31]='f',[32]='g',[33]='h',[34]='i',[35]='j',[36]='k',[37]='l',[38]='m',[39]='n',[40]='o',[41]='p',[42]='q',[43]='r',[44]='s',[45]='t',[46]='u',[47]='v',[48]='w',[49]='x',[50]='y',[51]='z',[52]='0',[53]='1',[54]='2',[55]='3',[56]='4',[57]='5',[58]='6',[59]='7',[60]='8',[61]='9',[62]='-',[63]='_'}
+
+-- function encode
+-- encodes input string to base64.
+function encode(data)
+	local bytes = {}
+	local result = ""
+	for spos=0,string.len(data)-1,3 do
+		for byte=1,3 do bytes[byte] = string.byte(string.sub(data,(spos+byte))) or 0 end
+		result = string.format('%s%s%s%s%s',result,base64chars[rsh(bytes[1],2)],base64chars[lor(lsh((bytes[1] % 4),4), rsh(bytes[2],4))] or "=",((#data-spos) > 1) and base64chars[lor(lsh(bytes[2] % 16,2), rsh(bytes[3],6))] or "=",((#data-spos) > 2) and base64chars[(bytes[3] % 64)] or "=")
+	end
+	return result
+end
 
 function isConfigCorrect(rid, rusername,rpassword) --checks if each line of the identification file as the right data 
     if rid == nil or rusername == nil or rpassword == nil then
@@ -77,7 +96,7 @@ end
 function accountToServer(id, username, password)
     local accountData = tostring(id)..";"..username..";"..password
     shell.setWorkingDirectory("/home/") -- if the server is down, internet.request will give an error so before trying it's going back to the basic dir 
-    if internet.request(urlAccount, encodeBase64(accountData))() == "Account accepted" then
+    if internet.request(urlAccount, encode(accountData))() == "Account accepted" then
         shell.setWorkingDirectory(workDirectory)
         return true
     else 
