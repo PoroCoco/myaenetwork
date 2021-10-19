a644f8de50795b4952614e4cb1d0b750e19864a5
PoroCoco <48033370+PoroCoco@users.noreply.github.com>
2021-10-19 16:48:21 +0200

    autoInstall now switch architecture

--- autoInstall.lua
+++ autoInstall.lua
@@ -1,18 +1,21 @@
 local shell = require("shell")
 local filesystem = require("filesystem")
+local computer = require("computer")
 
 if not filesystem.exists("/home/myaenetwork") then
 
     filesystem.makeDirectory("/home/myaenetwork")
     shell.setWorkingDirectory("/home/myaenetwork/")
 
-    print("downloading")
+    print("Downloading")
     shell.execute("wget https://raw.githubusercontent.com/PoroCoco/myaenetwork/main/account.lua")
     shell.execute("wget https://raw.githubusercontent.com/PoroCoco/myaenetwork/main/web.lua")
     shell.execute("wget https://raw.githubusercontent.com/PoroCoco/myaenetwork/main/MaenUpdater.lua")
     shell.setWorkingDirectory("/home/")
     filesystem.remove("home/autoInstall.lua")
-    print("done")
+    print("Done")
+    print("Switching computer architecture to Lua 5.3")
+    computer.setArchitecture("Lua 5.3")
 
 else
     print("Already installed")
