e8112d4e3cc668a2dbb8321777091d3bd63ab014
PoroCoco <48033370+PoroCoco@users.noreply.github.com>
2022-01-31 16:10:34 +0100

    switched to data card encoding and json format

--- MaenUpdater.lua
+++ MaenUpdater.lua
@@ -3,15 +3,14 @@ local filesystem = require("filesystem")
 local computer = require("computer")
 
 shell.setWorkingDirectory("/home/myaenetwork/")
-shell.execute("rm web.lua")
-shell.execute("rm account.lua")
+shell.execute("rm *.lua")
 
 print("Updating")
 shell.execute("wget https://raw.githubusercontent.com/PoroCoco/myaenetwork/main/account.lua")
 shell.execute("wget https://raw.githubusercontent.com/PoroCoco/myaenetwork/main/web.lua")
-shell.execute("wget https://raw.githubusercontent.com/PoroCoco/myaenetwork/main/accountAux.lua")
-shell.execute("wget https://raw.githubusercontent.com/PoroCoco/myaenetwork/main/webAux.lua")
 shell.execute("wget https://raw.githubusercontent.com/PoroCoco/myaenetwork/main/MaenUpdater.lua")
+shell.execute("wget https://raw.githubusercontent.com/rxi/json.lua/master/json.lua")
+
 shell.setWorkingDirectory("/home/")
 print("Rebooting")
 computer.shutdown(true)
